9e9aba035374e7f505f333de3bfd27b145814724
liuyuzhou <liuyuzhou@himobi.net>
2018-11-15 14:13:39 +0800

    内容：catch住LocationUtil中抛出的异常； 原因：输入的字符串中有非法字符； 方法：暂时catch住，待修复； 测试：自测；

--- app/src/main/java/com/ox/gpuimage/util/LocationUtil.java
+++ app/src/main/java/com/ox/gpuimage/util/LocationUtil.java
@@ -16,26 +16,31 @@ public class LocationUtil {
     public static String[] parseLocation(String location){
         if(TextUtils.isEmpty(location)){
             return null;
-        } else{
-            double longitude = 0.0;
-            double latitude = 0.0;
-            int index1 = location.lastIndexOf("-");
-            int index2 = location.lastIndexOf("+");
-            if(index1 != 0 && index1 != -1){
-                latitude = Double.valueOf(location.substring(0, index1));
-                longitude = Double.valueOf(location.substring(index1, location.length()));
-            } else if(index2 != 0 && index2 != -1){
-                latitude = Double.valueOf(location.substring(0, index2));
-                longitude = Double.valueOf(location.substring(index2, location.length()));
-            } else {
+        } else {
+            // TODO: 2018/11/15 location中存在非法字符时将抛出异常 
+            try {
+                double longitude = 0.0;
+                double latitude = 0.0;
+                int index1 = location.lastIndexOf("-");
+                int index2 = location.lastIndexOf("+");
+                if(index1 != 0 && index1 != -1){
+                    latitude = Double.valueOf(location.substring(0, index1));
+                    longitude = Double.valueOf(location.substring(index1, location.length()));
+                } else if(index2 != 0 && index2 != -1){
+                    latitude = Double.valueOf(location.substring(0, index2));
+                    longitude = Double.valueOf(location.substring(index2, location.length()));
+                } else {
+                    return null;
+                }
+                String[] result = new String[2];
+                //纬度
+                result[0] = Location.convert(latitude, Location.FORMAT_DEGREES);
+                //经度
+                result[1] = Location.convert(longitude, Location.FORMAT_DEGREES);
+                return result;
+            } catch (Exception e) {
                 return null;
             }
-            String[] result = new String[2];
-            //纬度
-            result[0] = Location.convert(latitude, Location.FORMAT_DEGREES);
-            //经度
-            result[1] = Location.convert(longitude, Location.FORMAT_DEGREES);
-            return result;
         }
     }
 }
